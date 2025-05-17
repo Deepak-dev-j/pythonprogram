3707b1209fec67b798d429302584033c4f137d64
Deepak-dev-j <22it21deepakj@gmail.com>
2025-05-17 11:26:22 +0530

    recursive funciton sample program

--- recursivefunction.py
+++ recursivefunction.py
@@ -1,1 +1,36 @@
-print("hello world")
+# # # # like a for loop
+
+# # # def cook(balance_maavu):
+# # #     if balance_maavu==1:
+# # #        return 1
+# # #     else:
+# # #         total_vadai=1+cook(balance_maavu-1)
+# # #         return total_vadai
+    
+# # # total_vadai=cook(4)
+# # # print("take this",total_vadai,"vadai")
+
+
+# # def cook(balance_maavu):
+# #     if balance_maavu==1:
+# #         return 1
+# #     else:
+# #         totalvadai=1+cook(balance_maavu-1)
+# #         return totalvadai
+# # totalvadai=cook(4)
+# # print(totalvadai)
+
+# def fact(x):
+#     if x==1:
+#         return 1
+#     else:
+#         return (x*fact(x-1))
+# print(fact(int(input("enter the number:"))))
+
+
+def fact(x):
+    if x==1:
+        return 1
+    else:
+        return (x*(fact(x-1)))
+print(fact(int(input("ener the number:"))))
